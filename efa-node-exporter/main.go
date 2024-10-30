30bdc3356bf450587544591eb9e57509524b5615
Nathan Fisher <nfisher@junctionbox.ca>
2024-10-30 10:46:27 -0400

    Add annotations

--- efa-node-exporter/main.go
+++ efa-node-exporter/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"context"
 	"dagger.io/dagger"
+	"fmt"
 	"log/slog"
 	"os"
 	"time"
 )
 
-const platform = "linux/amd64"
+const (
+	platform            = "linux/amd64"
+	awsEfaVersion       = "1.30.0"
+	nodeExporterVersion = "v1.8.2"
+	procFsVersion       = "v0.14.0"
+	goVersion           = "1.23.2"
+)
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
@@ -31,16 +38,19 @@ func main() {
 			Dockerfile: "Dockerfile",
 			Platform:   platform,
 			BuildArgs: []dagger.BuildArg{
-				{"GOLANG_VERSION", "1.23.2"},
-				{"NODE_EXPORTER_VERSION", "v1.8.2"},
-				{"PROCFS_EXPORTER_VERSION", "v0.14.0"},
-				{"EFA_INSTALLER_VERSION", "1.30.0"},
+				{"GOLANG_VERSION", goVersion},
+				{"NODE_EXPORTER_VERSION", nodeExporterVersion},
+				{"PROCFS_EXPORTER_VERSION", procFsVersion},
+				{"EFA_INSTALLER_VERSION", awsEfaVersion},
 			}})
 
 	_, err = client.Container(dagger.ContainerOpts{Platform: platform}).
 		From("public.ecr.aws/docker/library/ubuntu:20.04").
 		WithFile("/bin/node_exporter", builder.File("/workspace/node_exporter/node_exporter")).
 		WithEntrypoint([]string{"/bin/node_exporter"}).
+		WithAnnotation("org.opencontainers.image.title", "efa-node-exporter").
+		WithAnnotation("org.opencontainers.image.url", "https://github.com/mlmon/exporters/").
+		WithAnnotation("org.opencontainers.image.version", fmt.Sprintf("go:%s;node-exporter:%s;aws-efa:%s,", goVersion, nodeExporterVersion, awsEfaVersion)).
 		Publish(ctx, "nrfisher/efa-node-exporter:latest")
 	if err != nil {
 		logger.Error("%v", err)
